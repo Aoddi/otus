8b81a46e091b2a0ae7931d8c049f087b1c386fcb
feoktistov_aa <aoddi@yandex.ru>
2026-06-25 00:36:19 +0500

    change bc to awk

--- sum.sh
+++ sum.sh
@@ -17,7 +17,7 @@ fi
 num1=${oneNum//,/.}
 num2=${twoNum//,/.}
 
-result=$(echo "$num1 + $num2" | bc -l)
+result=$(awk -v n1="$num1" -v n2="$num2" 'BEGIN { print n1 + n2 }')
 
 clean_result=$(printf "%g" "$result")
 
