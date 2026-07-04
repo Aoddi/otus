a397a9651717f24e5da2054fe3717cddaa8f4981
feoktistov_aa <aoddi@yandex.ru>
2026-07-04 17:00:11 +0500

    add check brackets

--- src/index.php
+++ src/index.php
@@ -1,4 +1,43 @@
 <?php
 
-var_dump(123111111);
-phpinfo();
+if (!isset($_POST['string']) || trim($_POST['string']) === '') {
+    errorResponse();
+}
+
+$str = trim($_POST['string']);
+$arr = str_split($str);
+$count = 0;
+
+foreach ($arr as $char) {
+    if ($char === '(') {
+        $count++;
+    }
+
+    if ($char === ')') {
+        $count--;
+    }
+
+    if ($count < 0) {
+        errorResponse();
+    }
+}
+
+if ($count === 0) {
+    successResponse();
+}
+
+errorResponse();
+
+function successResponse()
+{
+    http_response_code(200);
+    echo 'все ок';
+    exit();
+}
+
+function errorResponse()
+{
+    http_response_code(400);
+    echo 'ошибка';
+    exit();
+}
